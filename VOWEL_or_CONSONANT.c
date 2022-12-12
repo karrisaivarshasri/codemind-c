238178e14667be91e8b27542d8459404123feef1
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2022-12-12 18:17:12 +0530

    #include<stdio.h> int main() {     char c;     scanf("%c",&c);     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u') {         printf("VOWEL");     }     else printf("CONSONANT"); }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    char c;
+    scanf("%c",&c);
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u') {
+        printf("VOWEL");
+    }
+    else printf("CONSONANT");
+}
