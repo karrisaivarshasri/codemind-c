957d9f5fabf1b5102cf44ac36b83349fb22453d2
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2022-12-20 20:22:06 +0530

    #include<stdio.h> #include<math.h> int main() {     int h,m; float a,b;     scanf("%d:%d",&h,&m);     a=abs((30*h)-(5.5*m));     b=360-a;     if(a<b)         printf("%.1f",a); else     printf("%.1f",b); }

---
+++ Angle_Between_Hands.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int h,m; float a,b;
+    scanf("%d:%d",&h,&m);
+    a=abs((30*h)-(5.5*m));
+    b=360-a;
+    if(a<b)
+        printf("%.1f",a);
+else 
+    printf("%.1f",b);
+}
