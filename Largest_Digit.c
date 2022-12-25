065d20ca93d5c8d3d8f9ae52eff4c4a0a8ff7f4d
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2022-12-25 20:34:51 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,d,i,k,max=-999; scanf("%d",&n);     d=log10(n)+1;i=1;     while(i<=d) {         k=n%10;         if(k>max) max=k;n/=10;i++;     }     printf("%d",max); }

---
+++ Largest_Digit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int n,d,i,k,max=-999; scanf("%d",&n);
+    d=log10(n)+1;i=1;
+    while(i<=d) {
+        k=n%10;
+        if(k>max) max=k;n/=10;i++;
+    }
+    printf("%d",max);
+}
