5353379ae0f1cd7004d0e25774c25fb8026e466f
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2022-12-26 22:19:56 +0530

    #include<stdio.h> int fun(int n) {     int d,i,temp,c=0,cc=0;     temp=n;     while(n) {         d=n%10;         if(d==0) {             return 0;         }         if(temp%d==0) {             cc++;         }         c++;         n=n/10;     }     if(c==cc) {         return 1;     }     else {         return 0;     } } int main() {     int n1,n2,i;     scanf("%d%d",&n1,&n2);     for(i=n1;i<=n2;i++) {         if(fun(i)) {             printf("%d ",i);         }     } }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int fun(int n) {
+    int d,i,temp,c=0,cc=0;
+    temp=n;
+    while(n) {
+        d=n%10;
+        if(d==0) {
+            return 0;
+        }
+        if(temp%d==0) {
+            cc++;
+        }
+        c++;
+        n=n/10;
+    }
+    if(c==cc) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+int main() {
+    int n1,n2,i;
+    scanf("%d%d",&n1,&n2);
+    for(i=n1;i<=n2;i++) {
+        if(fun(i)) {
+            printf("%d ",i);
+        }
+    }
+}
