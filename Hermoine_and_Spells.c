86b5e0c0fbef81a303584115423240085c674944
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2022-12-26 22:57:24 +0530

    #include<stdio.h> int main() {     int a,b,c; scanf("%d%d%d",&a,&b,&c);     if(a>c&&b>c) printf("%d",a+b);     else if(b>a&&c>a) printf("%d",b+c);     else printf("%d",a+c); }

--- Hermoine_and_Spells.c
+++ Hermoine_and_Spells.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main() {
-    int a,b,c;
-    scanf("%d%d%d",&a,&b,&c);
+    int a,b,c; scanf("%d%d%d",&a,&b,&c);
     if(a>c&&b>c) printf("%d",a+b);
     else if(b>a&&c>a) printf("%d",b+c);
     else printf("%d",a+c);
