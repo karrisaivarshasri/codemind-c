e20941d87613a74fe4abd332378f7a388989fcd2
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2022-12-28 21:58:25 +0530

    #include<stdio.h> int main() {     int n,i,j; scanf("%d",&n);     for(i=1;i<=n;i++) {         for(j=1;j<=i;j++)         printf("%d",j); printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
-    int n; scanf("%d",&n); char c1,c2;
-    for(c1=65;c1<=(64+n);c1++) {
-        for(c2=65;c2<=(64+n);c2++)
-        printf("%c ",c1); printf("
+    int n,i,j; scanf("%d",&n);
+    for(i=1;i<=n;i++) {
+        for(j=1;j<=i;j++)
+        printf("%d",j); printf("
 ");
     }
 }
