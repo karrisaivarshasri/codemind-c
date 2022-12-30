ee72fb3ed02ee0b8d7f8173e813548dc1e5efe76
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2022-12-30 21:17:09 +0530

    #include<stdio.h> int main() {     int n,i,flag=0;     scanf("%d",&n);     for(i=1;i<n;i++) {         if(i*i==n)         flag=1;     }     if (flag==1) {         printf("True");     }     else {         printf("False");     } }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int n,i,flag=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++) {
+        if(i*i==n) 
+        flag=1;
+    }
+    if (flag==1) { 
+        printf("True");
+    }
+    else {
+        printf("False");
+    }
+}
