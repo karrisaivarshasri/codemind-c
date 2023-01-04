a1f407e4277847bea33c2a1b5231d0ca60bc7b6b
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2023-01-04 22:08:25 +0530

    #include<stdio.h> int main() {     int n,r;  scanf("%d",&n);     r=n%3; if(n%3==0)     printf("NORMAL");     else if(n%3==1) printf("HUGE");     else printf("SMALL");     return 0; }

--- Mario_and_Mushrooms.c
+++ Mario_and_Mushrooms.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main() {
-    int n,r; scanf("%d",&n);
-    r=n%3;
-    if(n%3==0) printf("NORMAL");
-        else if(n%3==1) printf("HUGE");
+    int n,r;  scanf("%d",&n);
+    r=n%3; if(n%3==0)
+    printf("NORMAL");
+    else if(n%3==1) printf("HUGE");
     else printf("SMALL");
+    return 0;
 }
