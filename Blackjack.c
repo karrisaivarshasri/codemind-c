4372399766ab183779486e36f720678be88accce
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2023-01-05 21:33:04 +0530

    #include<stdio.h> int main() {     int x,y,z; scanf("%d%d%d",&x,&y,&z);     z=21-(x+y);     if(z>10) printf("%d",-1);     else printf("%d",z);     return 0; }

--- Blackjack.c
+++ Blackjack.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main() {
-    int m,n,o; scanf("%d%d%d",&m,&n,&o);
-    o=21-(m+n);
-    if(o>10) printf("%d",-1);
-    else printf("%d",o);
+    int x,y,z; scanf("%d%d%d",&x,&y,&z);
+    z=21-(x+y);
+    if(z>10) printf("%d",-1);
+    else printf("%d",z);
+    return 0;
 }
