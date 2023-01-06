48700ea8ec80e7548b6aec0ac6f3c3a5cd2b73e5
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2023-01-06 20:35:14 +0530

    #include<stdio.h> int main() {     int a; scanf("%d",&a);     if(a%2==0) printf("Even");     else printf("Odd"); }

--- Even_or_Odd.c
+++ Even_or_Odd.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main() {
-    int n;
-    scanf("%d",&n);
-    if(n%2==0) printf("Even");
+    int a; scanf("%d",&a);
+    if(a%2==0) printf("Even");
     else printf("Odd");
 }
