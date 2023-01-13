f10838a52a6bde9c2c21040fa7089a10fff19c4d
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2023-01-13 15:22:32 +0530

    #include<stdio.h> int main() {     int n,t,i; scanf("%d",&n);     for(i=1;i<=n;i++) {         int fact=1,j;         scanf("%d",&t);         for(j=1;j<=t;j++) {             fact=fact*j;         }         printf("%d ",fact);     } }

---
+++ Small_Factorials.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int n,t,i; scanf("%d",&n);
+    for(i=1;i<=n;i++) {
+        int fact=1,j;
+        scanf("%d",&t);
+        for(j=1;j<=t;j++) {
+            fact=fact*j;
+        }
+        printf("%d
+",fact);
+    }
+}
