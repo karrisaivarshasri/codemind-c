8e425cc0d768b376e84fbcc16761faef761d4bd5
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2023-05-18 19:14:15 +0530

    #include<stdio.h> int main() {     int n,i,t,d,c=0,s=0,l=0; scanf("%d",&n);     t=n;     for(i=1;i<=n;i++) {         if(n%i==0) {         c++;         }     }     if(c==2) {         while(n) {             int k=0;             d=n%10;             n=n/10;             l++;             for(i=1;i<=d;i++) {                 if(d%i==0) {                     k++;                 }             }             if(k==2) {                 s++;             }         }     }     if(s==l && c==2) {         printf("Mega Prime");     }     else if(l!=s||c>2) {         printf("Not Mega Prime");     } }

--- mega_prime.c
+++ mega_prime.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int main() {
-    int n,i,t,c=0,d,s=0,l=0;
-    scanf("%d",&n);
+    int n,i,t,d,c=0,s=0,l=0; scanf("%d",&n);
     t=n;
     for(i=1;i<=n;i++) {
         if(n%i==0) {
-            c++;
+        c++;
         }
     }
     if(c==2) {
         while(n) {
-            int k=0;d=n%10;n=n/10;l++;
+            int k=0;
+            d=n%10;
+            n=n/10;
+            l++;
             for(i=1;i<=d;i++) {
                 if(d%i==0) {
                     k++;
@@ -21,7 +23,7 @@ int main() {
             }
         }
     }
-    if(s==l&& c==2) {
+    if(s==l && c==2) {
         printf("Mega Prime");
     }
     else if(l!=s||c>2) {
