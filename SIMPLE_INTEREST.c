24907dbcbe241d6c156b197e6fbdf448e1adb254
karrisaivarshasri <119729757+karrisaivarshasri@users.noreply.github.com>
2023-05-24 12:55:30 +0530

    #include<stdio.h> int main() {     int p,t,r,si;     scanf("%d%d%d",&p,&t,&r);     si=(p*t*r)/100;     printf("%d",si); }

--- SIMPLE_INTEREST.c
+++ SIMPLE_INTEREST.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() {
-    int p,r,t,s;
-    scanf("%d%d%d",&p,&r,&t);
-    s=(p*r*t)/100;
-    printf("%d",s);
+    int p,t,r,si;
+    scanf("%d%d%d",&p,&t,&r);
+    si=(p*t*r)/100;
+    printf("%d",si);
 }
